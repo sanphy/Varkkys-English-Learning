d9e1526e5a68461948ea3e3b2c60c8936dbfbd07
sanphy <sanphypi4@gmail.com>
2025-05-15 00:21:18 +0530

    settings

--- varkkys_english/varkkys_english/settings.py
+++ varkkys_english/varkkys_english/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "Candidate_Record"
+    "Candidate_Record",
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
