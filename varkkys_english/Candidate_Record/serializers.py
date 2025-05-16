29b3add0b859e9f63642dd3016ed1efc5b3d9b0c
sanphy <sanphypi4@gmail.com>
2025-05-16 11:33:45 +0530

    added serializeer

--- varkkys_english/Candidate_Record/serializers.py
+++ varkkys_english/Candidate_Record/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from .models import Candidate
+from django.contrib.auth.models import User
+
 
 class CandidateSerializer(serializers.ModelSerializer):
+    assigned_to = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+        allow_null=True,
+        required=False,
+    )
+
     class Meta:
         model = Candidate
         fields = '__all__'
